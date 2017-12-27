446d2534895e2eb5ff8fe82a835e8de1a9f48bf2
LiNk-NY <mramos09@gmail.com>
2017-12-27 16:38:06 -0500

    move checkSeqlevels to MultiOmicQC-checkers

--- R/MultiOmicQC-checkers.R
+++ R/MultiOmicQC-checkers.R
@@ -1,8 +1,30 @@
-#' @rdname MultiOmicQC-checkers
+.consistencyChecker <- function(x, name, FUN) {
+    if (!is(x, "MultiAssayExperiment"))
+        stop("Provide a 'MultiAssayExperiment' object")
+    hasRR <- hasRowRanges(x)
+    exps <- experiments(x)
+
+    funList <- lapply(exps[hasRR], function(y) {
+        FUN(rowRanges(y))
+    })
+    if (length(funList) == 1L) { return(TRUE) }
+    allConsistent <- all(duplicated(funList)[-1L])
+    if (!allConsistent) {
+        warning("Inconsistent '", name, "' found:\n",
+            "    ", Biobase::selectSome(paste(
+                unlist(funList[!duplicated(funList)]), collapse = ", ")))
+    }
+    allConsistent
+}
+
+#' @name MultiOmicQC-checkers
+#'
+#' @title Check seqinfo consistency across ranged experiments
 #'
 #' @description A set of functions to check several aspects of ranged
 #' experiments including \code{seqlengths}, \code{seqlevels} and
-#' \code{seqlevelsStyle}.
+#' \code{seqlevelsStyle}. These group of functions return a logical scalar
+#' value when the names, styles, etc. are consistent accross all experiments.
 #'
 #' @section checkSeqlengths:
 #' Runs and checks \code{seqlengths} on all ranged experiments.
@@ -23,9 +45,18 @@ checkSeqlengths <- function(x) {
     .consistencyChecker(x, "seqlengths", seqlengths)
 }
 
-#' @name MultiOmicQC-checkers
+#' @rdname MultiOmicQC-checkers
 #'
 #' @export
 checkSeqlevelsStyle <- function(x) {
     .consistencyChecker(x, "seqlevelsStyle", seqlevelsStyle)
 }
+
+#' @rdname MultiOmicQC-checkers
+#'
+#' @export
+checkSeqlevels <- function(x) {
+    .consistencyChecker(x, "seqlevels", seqlevels)
+}
+
+
