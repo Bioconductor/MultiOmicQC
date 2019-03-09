a32a3b2afb0909f15327b6fd3320ada94d283c4c
LiNk-NY <mramos09@gmail.com>
2019-03-08 19:28:26 -0500

    add setter function for seqlevelsStyle

---
+++ R/MultiOmicQC-setters.R
@@ -0,0 +1,24 @@
+.setter <- function(x, name, FUN, value) {
+    if (!is(x, "MultiAssayExperiment"))
+        stop("Provide a 'MultiAssayExperiment' object")
+    hasRR <- hasRowRanges(x)
+    exps <- experiments(x)
+
+    exps[hasRR] <- lapply(exps[hasRR], function(y) {
+        FUN(y, value)
+    })
+
+    BiocGenerics:::replaceSlots(x, ExperimentList = exps, check = FALSE)
+}
+
+#' @rdname MultiOmicQC-setters
+#'
+#' @title Utility functions for setting metadata in MultiAssayExperiment
+#'
+#' @param x A \linkS4class{MultiAssayExperiment}
+#' @param style A character vector with a single element to specify the style
+#'
+#' @export
+setSeqlevelsStyle <- function(x, style) {
+    .setter(x, "seqlevelsStyle", GenomeInfoDb::`seqlevelsStyle<-`, style)
+}
